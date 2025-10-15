68502536125de235afdce2131ad278d1844dfce9
Bhaskar <19221a04g7@bvcgroup.in>
2025-10-15 23:57:45 +0530

    stable version

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  basePath: isProd ? '/voter-management' : '',
-  // other config options
-};
